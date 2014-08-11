c4d8fde5b09a599f60b8cdc5b40bbd83fb480be4
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-08-11 15:02:49 -0500

    Add the setup.py script
    A trivial setup.py script has been added, which installs the morsefit
    package and the main driver script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='morsefit',
+        version='0.1',
+        description='Fit ab-initio PES to Morse potential',
+        author='Tschijnmo TSCHAU',
+        author_email='tschijnmotschau@gmail.com'
+        url='https://github.com/tschijnmo/morsefit',
+        packages=['morsefit'],
+        scripts=['scripts/morsefit']
+        )
+
